fe5a725fa3c82021afeba1400dc14f55d3db847d
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-03-14 09:53:13 +0100

    Bulk check feature.

--- TrackMagic.Application/Features/Cards/CardsService.cs
+++ TrackMagic.Application/Features/Cards/CardsService.cs
@@ -4,10 +4,23 @@ using TrackMagic.Domain.Entities;
 
 namespace TrackMagic.Application.Features.Cards
 {
-    public interface ICardsService : IFeatureService<Card> { }
+    public interface ICardsService : IFeatureService<Card>
+    {
+        Task<bool> AllExistAsync(List<int> cardIds, CancellationToken cancellationToken);
+    }
 
     public class CardsService : BaseFeatureService<Card>, ICardsService
     {
         public CardsService(IAppDbContext dbContext) : base(dbContext) { }
+
+        public async Task<bool> AllExistAsync(List<int> cardIds, CancellationToken cancellationToken)
+        {
+            foreach (int id in cardIds)
+            {
+                if (!await ExistsAsync(c => c.Id == id, cancellationToken)) return false;
+            }
+
+            return true;
+        }
     }
 }
