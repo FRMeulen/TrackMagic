5361a664674cac4ec6ec51302e3c7d8d5f6e830f
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-03-19 10:55:53 +0100

    Expose ChangeTracker.

--- TrackMagic.Application/Common/Persistence/IAppDbContext.cs
+++ TrackMagic.Application/Common/Persistence/IAppDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using TrackMagic.Domain.Contracts;
 
 namespace TrackMagic.Application.Common.Persistence
 {
     public interface IAppDbContext
     {
+        ChangeTracker ChangeTracker { get; }
         DatabaseFacade Database { get; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
         DbSet<T> Set<T>() where T : class;
