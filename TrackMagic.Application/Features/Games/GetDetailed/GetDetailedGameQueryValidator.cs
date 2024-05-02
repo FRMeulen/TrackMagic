82b77eacc30b3b652e1af27c016e93f91739a310
Falco van der Meulen <fme@brunelitsolutions.nl>
2024-05-02 14:11:31 +0200

    GetDetailedGameQuery validator.

--- TrackMagic.Application/Features/Games/GetDetailed/GetDetailedGameQueryValidator.cs
+++ TrackMagic.Application/Features/Games/GetDetailed/GetDetailedGameQueryValidator.cs
@@ -1,6 +1,18 @@
-﻿namespace TrackMagic.Application.Features.Games.GetDetailed
+﻿using FluentValidation;
+using TrackMagic.Domain.Entities;
+using TrackMagic.Shared.Constants;
+
+namespace TrackMagic.Application.Features.Games.GetDetailed
 {
-    internal class GetDetailedGameQueryValidator
+    public class GetDetailedGameQueryValidator : AbstractValidator<GetDetailedGameQuery>
     {
+        public GetDetailedGameQueryValidator(IGamesService gamesService)
+        {
+            RuleFor(g => g.Id)
+                .NotEmpty()
+                .MustAsync(async (id, cancellationToken)
+                    => await gamesService.ExistsAsync(g => g.Id == id, cancellationToken))
+                .WithMessage((_) => DefaultMessages.MustExistMessage(nameof(Game), nameof(Game.Id), $"{_.Id}"));
+        }
     }
 }
